010944fcf6d4f201f99f97713b6e1922d37672d4
Antonio Anderson Souza <antonioams@gmail.com>
2011-08-15 10:28:21 -0300

    Implementação da segunda dimensão e creche

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -7,27 +7,29 @@ class FlowController < ApplicationController
     @institution = params[:institution]
     if(params[:password] == '123456' && @institution == "EMEF ALFREDO FARHAT, DEPUTADO")
       @segment = "Familiares"
-      render "confirm"
+      @institution_type = "EMEF"
     elsif(params[:password] == '1qa0pk' && @institution == "EMEF ALICE RABECHINI FERREIRA")
       @segment = "Professores e Gestores"
-      render "confirm"
+      @institution_type = "EMEF"
     elsif(params[:password] == '2ws9om' && @institution == "EMEF ALIPIO DA SILVA LAVOURA, PROF.")
       @segment = "Funcionarios"
-      render "confirm"
+      @institution_type = "EMEF"
     elsif(params[:password] == '3ed8ik' && @institution == "EMEF ANEZIO CABRAL, PROF.")
-      @segment = "Educando"
-      render "confirm"
+      @segment = "Educandos"
+      @institution_type = "EMEF"
     elsif(params[:password] == '4rf7uj' && @institution == "CRECHE ALHA ELIAS ABIBE")
       @segment = "Familiares"
-      render "confirm"
+      @institution_type = "CRECHE"
     elsif(params[:password] == '5tg6yh' && @institution == "CRECHE ALZIRA SILVA MEDEIROS")
       @segment = "Professores e Gestores"
-      render "confirm"
+      @institution_type = "CRECHE"
     elsif(params[:password] == 'zxcmnb' && @institution == "CRECHE AMELIA TOZZETO VIVIANE")
       @segment = "Funcionarios"
-      render "confirm"
-    elsif(params[:password] == 'zxc098' && @institution == "CRECHE BENEDITA DE OLIVEIRA")
-      @segment = "Educando"
+      @institution_type = "CRECHE"
+    end
+
+    if !@segment.nil?
+      generate_data
       render "confirm"
     else
       generate_institutions
@@ -37,7 +39,9 @@ class FlowController < ApplicationController
 
   def identify
     @institution = params[:institution]
+    @institution_type = params[:institution_type]
     @segment = params[:segment]
+    generate_data
     if(params[:commit] == 'sim')
       render "identify"
     else
@@ -48,33 +52,41 @@ class FlowController < ApplicationController
 
   def instructions
     @institution = params[:institution]
+    @institution_type = params[:institution_type]
     @segment = params[:segment]
     @name = params[:name]
+    generate_data
     render "instructions"
   end
 
   def answerdimension
     @institution = params[:institution]
+    @institution_type = params[:institution_type]
     @segment = params[:segment]
     @name = params[:name]
-    generate_questions
+    @dimension = params[:dimension]
+    generate_data
     render "answer"
   end
 
   def review
     @institution = params[:institution]
+    @institution_type = params[:institution_type]
     @segment = params[:segment]
     @name = params[:name]
-    generate_questions
+    @dimension = params[:dimension]
+    generate_data
     render "review"
   end
 
   def checkreview
     @institution = params[:institution]
+    @institution_type = params[:institution_type]
     @segment = params[:segment]
     @name = params[:name]
+    @dimension = params[:dimension]
+    generate_data
     if(params[:commit] == 'modificar')
-      generate_questions
       render "answer"
     else
       render "save"
@@ -163,7 +175,45 @@ private
     @institutions
   end
 
-  def generate_questions
+  def generate_data
+    @steps = []
+    @steps << ["Autenticacao", "Autenticacao"]
+    @steps << ["Confirmacao segmento", "Confirmacao segmento"]
+    @steps << ["Identificacao","Identificacao"]
+    @steps << ["Instrucoes basicas", "Instrucoes basicas"]
+    @steps << ["Ambiente ducativo", "Dimensao 1"]
+    @steps << ["Ambiente fisico escolar e materiais", "Dimensao 2"]
+    @steps << ["Planejamento institucional e pratica pedagogica", "Dimensao 3"]
+    @steps << ["Avaliacao", "Dimensao 4"]
+    @steps << ["Acesso e permanência dos educandos na escola", "Dimensao 5"]
+    @steps << ["Promocao da saude", "Dimensao 6"]
+    @steps << ["Educacao socioambiental e praticas ecopedagogicas", "Dimensao 7"]
+    @steps << ["Envolvimento com as familias e participacao na rede de protecao social", "Dimensao 8"]
+    @steps << ["Gestao escolar democratica", "Dimensao 9"]
+    @steps << ["Formação e condições de trabalho dos profissionais da escola", "Dimensao 10"]
+    @steps << ["Processos de alfabetização e letramento", "Dimensao 11"]
+
+    @dimensions = []
+    @dimensions << "Ambiente educativo"
+    @dimensions << "Ambiente fisico escolar e materiais"
+    @dimensions << "Planejamento institucional e prática pedagógica"
+    @dimensions << "Avaliação"
+    @dimensions << "Acesso e permanência dos educandos na escola"
+    @dimensions << "Promoção da saúde"
+    @dimensions << "Educação socioambiental e práticas ecopedagógicas"
+    @dimensions << "Envolvimento com as famílias e participação na rede de proteção social"
+    @dimensions << "Gestão escolar democrática" 
+    @dimensions << "Formação e condições de trabalho dos profissionais da escola"
+    @dimensions << "Processos de alfabetização e letramento"
+    
+    if params[:institution_type] == "EMEF"
+      generate_emef_questions
+    else
+      generate_creche_questions
+    end
+  end
+  
+  def generate_emef_questions
     @questions = []
     @questions[0] = []
     @questions[0][0] = {}
@@ -301,5 +351,279 @@ private
     @questions[0][9]["Funcionarios"] << 1
     @questions[0][9]["Professores e Gestores"] = []
     @questions[0][9]["Professores e Gestores"] << 1
+
+    @questions[1] = []
+    @questions[1][0] = {}
+    @questions[1][0]["Educandos"] = []
+    @questions[1][0]["Educandos"] << 1
+    @questions[1][0]["Educandos"] << 2
+    @questions[1][0]["Educandos"] << 3
+    @questions[1][0]["Educandos"] << 4
+    @questions[1][0]["Educandos"] << 5
+    @questions[1][0]["Familiares"] = []
+    @questions[1][0]["Familiares"] << 1
+    @questions[1][0]["Familiares"] << 2
+    @questions[1][0]["Familiares"] << 3
+    @questions[1][0]["Familiares"] << 4
+    @questions[1][0]["Familiares"] << 5
+    @questions[1][0]["Funcionarios"] = []
+    @questions[1][0]["Funcionarios"] << 1
+    @questions[1][0]["Funcionarios"] << 2
+    @questions[1][0]["Funcionarios"] << 3
+    @questions[1][0]["Funcionarios"] << 4
+    @questions[1][0]["Funcionarios"] << 5
+    @questions[1][0]["Professores e Gestores"] = []
+    @questions[1][0]["Professores e Gestores"] << 1
+    @questions[1][0]["Professores e Gestores"] << 2
+    @questions[1][0]["Professores e Gestores"] << 3
+    @questions[1][0]["Professores e Gestores"] << 4
+    @questions[1][0]["Professores e Gestores"] << 5
+    @questions[1][1] = {}
+    @questions[1][1]["Educandos"] = []
+    @questions[1][1]["Educandos"] << 1
+    @questions[1][1]["Educandos"] << 2
+    @questions[1][1]["Educandos"] << 3
+    @questions[1][1]["Familiares"] = []
+    @questions[1][1]["Familiares"] << 1
+    @questions[1][1]["Familiares"] << 2
+    @questions[1][1]["Funcionarios"] = []
+    @questions[1][1]["Funcionarios"] << 1
+    @questions[1][1]["Funcionarios"] << 2
+    @questions[1][1]["Funcionarios"] << 3
+    @questions[1][1]["Professores e Gestores"] = []
+    @questions[1][1]["Professores e Gestores"] << 1
+    @questions[1][1]["Professores e Gestores"] << 2
+    @questions[1][2] = {}
+    @questions[1][2]["Educandos"] = []
+    @questions[1][2]["Educandos"] << 1
+    @questions[1][2]["Educandos"] << 2
+    @questions[1][2]["Educandos"] << 3
+    @questions[1][2]["Familiares"] = []
+    @questions[1][2]["Familiares"] << 1
+    @questions[1][2]["Familiares"] << 2
+    @questions[1][2]["Familiares"] << 3
+    @questions[1][2]["Funcionarios"] = []
+    @questions[1][2]["Funcionarios"] << 1
+    @questions[1][2]["Funcionarios"] << 2
+    @questions[1][2]["Funcionarios"] << 3
+    @questions[1][2]["Professores e Gestores"] = []
+    @questions[1][2]["Professores e Gestores"] << 1
+    @questions[1][2]["Professores e Gestores"] << 2
+    @questions[1][2]["Professores e Gestores"] << 3
+    @questions[1][3] = {}
+    @questions[1][3]["Educandos"] = []
+    @questions[1][3]["Familiares"] = []
+    @questions[1][3]["Familiares"] << 3
+    @questions[1][3]["Funcionarios"] = []
+    @questions[1][3]["Funcionarios"] << 1
+    @questions[1][3]["Funcionarios"] << 2
+    @questions[1][3]["Funcionarios"] << 3
+    @questions[1][3]["Professores e Gestores"] = []
+    @questions[1][3]["Professores e Gestores"] << 1
+    @questions[1][3]["Professores e Gestores"] << 2
+    @questions[1][3]["Professores e Gestores"] << 3
+    
   end
+
+  def generate_creche_questions
+    @questions = []
+    @questions[0] = []
+    @questions[0][0] = {}
+    @questions[0][0]["Familiares"] = []
+    @questions[0][0]["Familiares"] << 1
+    @questions[0][0]["Familiares"] << 2
+    @questions[0][0]["Funcionarios"] = []
+    @questions[0][0]["Funcionarios"] << 1
+    @questions[0][0]["Funcionarios"] << 2
+    @questions[0][0]["Professores e Gestores"] = []
+    @questions[0][0]["Professores e Gestores"] << 1
+    @questions[0][0]["Professores e Gestores"] << 2
+    @questions[0][1] = {}
+    @questions[0][1]["Familiares"] = []
+    @questions[0][1]["Familiares"] << 1
+    @questions[0][1]["Familiares"] << 2
+    @questions[0][1]["Familiares"] << 3
+    @questions[0][1]["Funcionarios"] = []
+    @questions[0][1]["Funcionarios"] << 1
+    @questions[0][1]["Funcionarios"] << 2
+    @questions[0][1]["Funcionarios"] << 3
+    @questions[0][1]["Professores e Gestores"] = []
+    @questions[0][1]["Professores e Gestores"] << 1
+    @questions[0][1]["Professores e Gestores"] << 2
+    @questions[0][1]["Professores e Gestores"] << 3
+    @questions[0][2] = {}
+    @questions[0][2]["Familiares"] = []
+    @questions[0][2]["Familiares"] << 1
+    @questions[0][2]["Familiares"] << 2
+    @questions[0][2]["Familiares"] << 3
+    @questions[0][2]["Funcionarios"] = []
+    @questions[0][2]["Funcionarios"] << 1
+    @questions[0][2]["Funcionarios"] << 2
+    @questions[0][2]["Funcionarios"] << 3
+    @questions[0][2]["Professores e Gestores"] = []
+    @questions[0][2]["Professores e Gestores"] << 1
+    @questions[0][2]["Professores e Gestores"] << 2
+    @questions[0][2]["Professores e Gestores"] << 3
+    @questions[0][3] = {}
+    @questions[0][3]["Familiares"] = []
+    @questions[0][3]["Familiares"] << 2
+    @questions[0][3]["Familiares"] << 3
+    @questions[0][3]["Funcionarios"] = []
+    @questions[0][3]["Funcionarios"] << 1
+    @questions[0][3]["Funcionarios"] << 2
+    @questions[0][3]["Funcionarios"] << 3
+    @questions[0][3]["Professores e Gestores"] = []
+    @questions[0][3]["Professores e Gestores"] << 1
+    @questions[0][3]["Professores e Gestores"] << 2
+    @questions[0][3]["Professores e Gestores"] << 3
+    @questions[0][4] = {}
+    @questions[0][4]["Familiares"] = []
+    @questions[0][4]["Familiares"] << 1
+    @questions[0][4]["Familiares"] << 3
+    @questions[0][4]["Familiares"] << 4
+    @questions[0][4]["Funcionarios"] = []
+    @questions[0][4]["Funcionarios"] << 1
+    @questions[0][4]["Funcionarios"] << 2
+    @questions[0][4]["Funcionarios"] << 4
+    @questions[0][4]["Professores e Gestores"] = []
+    @questions[0][4]["Professores e Gestores"] << 1
+    @questions[0][4]["Professores e Gestores"] << 2
+    @questions[0][4]["Professores e Gestores"] << 3
+    @questions[0][4]["Professores e Gestores"] << 4
+    @questions[0][5] = {}
+    @questions[0][5]["Familiares"] = []
+    @questions[0][5]["Familiares"] << 1
+    @questions[0][5]["Familiares"] << 2
+    @questions[0][5]["Funcionarios"] = []
+    @questions[0][5]["Funcionarios"] << 1
+    @questions[0][5]["Funcionarios"] << 2
+    @questions[0][5]["Professores e Gestores"] = []
+    @questions[0][5]["Professores e Gestores"] << 1
+    @questions[0][5]["Professores e Gestores"] << 2
+    @questions[0][6] = {}
+    @questions[0][6]["Familiares"] = []
+    @questions[0][6]["Familiares"] << 1
+    @questions[0][6]["Familiares"] << 2
+    @questions[0][6]["Funcionarios"] = []
+    @questions[0][6]["Funcionarios"] << 1
+    @questions[0][6]["Funcionarios"] << 2
+    @questions[0][6]["Professores e Gestores"] = []
+    @questions[0][6]["Professores e Gestores"] << 1
+    @questions[0][6]["Professores e Gestores"] << 2
+    @questions[0][7] = {}
+    @questions[0][7]["Familiares"] = []
+    @questions[0][7]["Familiares"] << 1
+    @questions[0][7]["Familiares"] << 2
+    @questions[0][7]["Familiares"] << 3
+    @questions[0][7]["Familiares"] << 4
+    @questions[0][7]["Funcionarios"] = []
+    @questions[0][7]["Funcionarios"] << 1
+    @questions[0][7]["Funcionarios"] << 2
+    @questions[0][7]["Funcionarios"] << 3
+    @questions[0][7]["Funcionarios"] << 4
+    @questions[0][7]["Professores e Gestores"] = []
+    @questions[0][7]["Professores e Gestores"] << 1
+    @questions[0][7]["Professores e Gestores"] << 2
+    @questions[0][7]["Professores e Gestores"] << 3
+    @questions[0][7]["Professores e Gestores"] << 4
+    @questions[0][8] = {}
+    @questions[0][8]["Familiares"] = []
+    @questions[0][8]["Familiares"] << 1
+    @questions[0][8]["Familiares"] << 2
+    @questions[0][8]["Familiares"] << 3
+    @questions[0][8]["Funcionarios"] = []
+    @questions[0][8]["Funcionarios"] << 1
+    @questions[0][8]["Funcionarios"] << 2
+    @questions[0][8]["Funcionarios"] << 3
+    @questions[0][8]["Professores e Gestores"] = []
+    @questions[0][8]["Professores e Gestores"] << 1
+    @questions[0][8]["Professores e Gestores"] << 2
+    @questions[0][8]["Professores e Gestores"] << 3
+    @questions[0][9] = {}
+    @questions[0][9]["Familiares"] = []
+    @questions[0][9]["Funcionarios"] = []
+    @questions[0][9]["Funcionarios"] << 1
+    @questions[0][9]["Funcionarios"] << 2
+    @questions[0][9]["Professores e Gestores"] = []
+    @questions[0][9]["Professores e Gestores"] << 1
+    @questions[0][9]["Professores e Gestores"] << 2
+    @questions[0][10] = {}
+    @questions[0][10]["Familiares"] = []
+    @questions[0][10]["Familiares"] << 2
+    @questions[0][10]["Familiares"] << 3
+    @questions[0][10]["Funcionarios"] = []
+    @questions[0][10]["Funcionarios"] << 1
+    @questions[0][10]["Funcionarios"] << 2
+    @questions[0][10]["Funcionarios"] << 3
+    @questions[0][10]["Funcionarios"] << 4
+    @questions[0][10]["Professores e Gestores"] = []
+    @questions[0][10]["Professores e Gestores"] << 1
+    @questions[0][10]["Professores e Gestores"] << 2
+    @questions[0][10]["Professores e Gestores"] << 3
+    @questions[0][10]["Professores e Gestores"] << 4
+
+    @questions[1] = []
+    @questions[1][0] = {}
+    @questions[1][0]["Familiares"] = []
+    @questions[1][0]["Familiares"] << 1
+    @questions[1][0]["Familiares"] << 2
+    @questions[1][0]["Familiares"] << 3
+    @questions[1][0]["Familiares"] << 4
+    @questions[1][0]["Funcionarios"] = []
+    @questions[1][0]["Funcionarios"] << 1
+    @questions[1][0]["Funcionarios"] << 2
+    @questions[1][0]["Funcionarios"] << 3
+    @questions[1][0]["Funcionarios"] << 4
+    @questions[1][0]["Professores e Gestores"] = []
+    @questions[1][0]["Professores e Gestores"] << 1
+    @questions[1][0]["Professores e Gestores"] << 2
+    @questions[1][0]["Professores e Gestores"] << 3
+    @questions[1][0]["Professores e Gestores"] << 4
+    @questions[1][1] = {}
+    @questions[1][1]["Familiares"] = []
+    @questions[1][1]["Familiares"] << 1
+    @questions[1][1]["Familiares"] << 2
+    @questions[1][1]["Familiares"] << 3
+    @questions[1][1]["Familiares"] << 4
+    @questions[1][1]["Funcionarios"] = []
+    @questions[1][1]["Funcionarios"] << 1
+    @questions[1][1]["Funcionarios"] << 2
+    @questions[1][1]["Funcionarios"] << 3
+    @questions[1][1]["Funcionarios"] << 4
+    @questions[1][1]["Professores e Gestores"] = []
+    @questions[1][1]["Professores e Gestores"] << 1
+    @questions[1][1]["Professores e Gestores"] << 2
+    @questions[1][1]["Professores e Gestores"] << 3
+    @questions[1][1]["Professores e Gestores"] << 4
+    @questions[1][2] = {}
+    @questions[1][2]["Familiares"] = []
+    @questions[1][2]["Familiares"] << 1
+    @questions[1][2]["Familiares"] << 2
+    @questions[1][2]["Familiares"] << 3
+    @questions[1][2]["Familiares"] << 4
+    @questions[1][2]["Funcionarios"] = []
+    @questions[1][2]["Funcionarios"] << 1
+    @questions[1][2]["Funcionarios"] << 2
+    @questions[1][2]["Funcionarios"] << 3
+    @questions[1][2]["Funcionarios"] << 4
+    @questions[1][2]["Professores e Gestores"] = []
+    @questions[1][2]["Professores e Gestores"] << 1
+    @questions[1][2]["Professores e Gestores"] << 2
+    @questions[1][2]["Professores e Gestores"] << 3
+    @questions[1][2]["Professores e Gestores"] << 4
+    @questions[1][3] = {}
+    @questions[1][3]["Familiares"] = []
+    @questions[1][3]["Familiares"] << 3
+    @questions[1][3]["Familiares"] << 4
+    @questions[1][3]["Funcionarios"] = []
+    @questions[1][3]["Funcionarios"] << 1
+    @questions[1][3]["Funcionarios"] << 2
+    @questions[1][3]["Funcionarios"] << 3
+    @questions[1][3]["Professores e Gestores"] = []
+    @questions[1][3]["Professores e Gestores"] << 1
+    @questions[1][3]["Professores e Gestores"] << 2
+    @questions[1][3]["Professores e Gestores"] << 3
+
+  end
+
 end
