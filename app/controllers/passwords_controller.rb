76e7e06ff32bfc26a82935cdd56ab4e8fc7a3fdb
Fabrício Ferrari de Campos <contato@vizir.com.br>
2012-09-10 11:03:28 -0300

    atualizada carta de senhas

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -21,17 +21,16 @@ class PasswordsController < ApplicationController
     respond_to do |format|
       format.html {render :layout => false}
       format.pdf do
-        headless = Headless.new
-        headless.start
+        #headless = Headless.new
+        #headless.start
         render :layout => false, :pdf => "password_letters"
-        headless.destroy
+        #headless.destroy
       end
     end
   end
 
   def generate_all_passwords
-    pg = IPF::PasswordGenerator.new
-    pg.generate
+    IPF::PasswordGenerator.new.generate_all
     redirect_to admin_passwords_path, :notice => "Senhas geradas com sucesso."
   end
 end
