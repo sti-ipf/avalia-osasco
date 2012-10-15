b6f7e9fcfca67a322a3975695d15618ac66f3690
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2012-10-15 18:44:26 -0300

    adicionado script que arruma os indicadores

--- script/fix_indicators_name.rb
+++ script/fix_indicators_name.rb
@@ -1,7 +1,11 @@
 Indicator.all.each do |i|
-  name = i.name[4..i.name.length]
+  name = i.name
 
   while(%w(1, 2, 3, 4, 5, 6, 7, 8, 9, 0).include?(name[0]))
+    name = name[2..name.length]
+  end
+
+  while(name[0]= ".")
     name = name[2..name.length]
   end
   i.name = name
